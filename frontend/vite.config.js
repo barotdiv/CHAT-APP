b06378f1a852453708be0a49b89e3574356bb613
barotdiv <divbarot15@gmail.com>
2026-07-10 17:15:19 +0530

    modify changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "192.168.1.24",
+    host: "0.0.0.0",
     port: 5173,
   },
 });
