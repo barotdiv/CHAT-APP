e5aeb18da7a5ed1d5ba7864efcd5b0966d81e52b
barotdiv <divbarot15@gmail.com>
2026-07-13 13:02:14 +0530

    New files added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/api/status", (req, res) => {
     res.json({ message: "Backend server is runnig successfully!" });
