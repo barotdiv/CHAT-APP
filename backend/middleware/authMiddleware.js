d38c9034373edd43eef04c66067b19600c741cb1
barotdiv <divbarot15@gmail.com>
2026-07-13 16:03:40 +0530

    modify file

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,31 +4,21 @@ import User from "../models/User.js";
 export const protect = async (req, res, next) => {
     let token;
 
-    console.log("---MIDDLEWARE TRIGGERED ---");
-    console.log("Authorization Header:", req.headers.authorization);
-
-    if (req.headers.autorization && req.headers.autorization.trim().includes("Bearer")) {
+    if (req.headers.autorization) {
         try {
-            token = req.headers.authorization.split(' ')[1];
-            consolelog("Token extracted successfully.");
+            const parts = req.headers.autorization.trim().split(' ');
+            token = parts[parts.length - 1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            console.log("Token decoded successfully:", decoded);
             req.user = await User.findById(decoded.id).select("-password");
-            console.log("User found in database:", req.user ? "Yes" : "No");
 
             if (!req.user) {
-                console.log("Error: Token is valid, but user no longer exists in DB.");
                 return res.status(401).json({ message: 'Not authorized, user not found' });
             }
-            console.log("Middleware passed! Moving to route...");
             return next();
         } catch (error) {
-            console.error("JWT Verification Error:", error.message);
+            console.error("JWT Error:", error.message);
             return res.status(401).json({ message: "Not authorized token failed" });
         }
     }
-    if (!token) {
-        console.log("Error: No token was found in the header");
-        return res.status(401).json({ message: "Not authorized, no token" });
-    }
+    return res.status(401).json({ message: "Not authorized, no token" });
 };
