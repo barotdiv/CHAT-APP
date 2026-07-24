3043e761369035a12a893c435409fdc75487352c
barotdiv <divbarot15@gmail.com>
2026-07-24 16:48:28 +0530

    initialize React root with router, theme, auth, and settings context providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,15 +7,18 @@ import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext'
 import { ThemeProvider } from './context/ThemeContext'
+import { SettingsProvider } from './context/SettingsContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
         <AuthProvider>
-          <LinkProvider component={Link}>
-            <App />
-          </LinkProvider>
+          <SettingsProvider>
+            <LinkProvider component={Link}>
+              <App />
+            </LinkProvider>
+          </SettingsProvider>
         </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
